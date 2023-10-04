8d1611526fa3a77baa9323713ba278cf0210d0ca
Mubarek Shikur <manmuba90@gmail.com>
2023-10-04 10:50:16 +0300

    initial

---
+++ python-lists-hackerrank.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    N = int(input())
+    l=[]
+    for i in range(N):
+        x=input().split()
+        if x[0] == 'insert':
+            l.insert(int(x[1]), int(x[2]))
+        elif x[0] == 'append':
+            l.append(int(x[1]))
+        elif x[0] == 'pop':
+            l.pop()
+        elif x[0] == 'print':
+            print(l)
+        elif x[0] == 'sort':
+            l.sort()
+        elif x[0] == 'reverse':
+            l.reverse()
+        elif x[0] == 'remove':
+            l.remove(int(x[1]))
+                
